ef12ffb18cdc2984fea554cec9a6cc24e9538711
Sean Francis N. Ballais <seanballais@pop-os.localdomain>
2023-11-05 02:06:47 +0800

    Add initial work on the Linux port of the build script.

---
+++ build_all.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "[NOT PORTED] Build Dagor tools..."
+echo "[NOT PORTED] Build Dargbox vfsroms and shaders..."
+echo "[NOT PORTED] Build Physics (Jolt) Test..."
+echo "[NOT PORTED] Build Skies sample..."
+echo "[NOT PORTED] Build GI Test sample..."
