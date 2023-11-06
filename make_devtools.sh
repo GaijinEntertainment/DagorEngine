9ac8b79ae822f16e16b0b04aa5cfbe83ad031144
Sean Francis N. Ballais <sean@seanballais.com>
2023-11-07 02:10:27 +0800

    Complete dev tools directory check in make_devtools.sh.

--- make_devtools.sh
+++ make_devtools.sh
@@ -10,9 +10,26 @@ fi
 function error() {
     echo "" >&2
     echo "ERROR: $1" >&2
-    echo ""
+    echo "" >&2
 
     exit 1
+}
+
+function download_and_install_jam() {
+    # Check first if we are using Ubuntu/Pop!_OS. Let's support
+    # only Ubuntu for now. Contributions welcome.
+    . /etc/os-release
+    OS=$ID
+    OS_LIKE=$ID_LIKE
+
+    if [[ $OS != *"ubuntu"* ]] && [[ $OS_LIKE != *"ubuntu"* ]]; then
+        error "Script does not support non-Ubuntu distributions yet."
+    fi
+
+    wget -q "https://github.com/GaijinEntertainment/jam-G8/releases/download/2.5-G8-1.2-2023%2F05%2F04/jam-ubuntu-20.04-x64.tar.gz"
+    tar -xvf "jam-ubuntu-20.04-x64.tar.gz" > /dev/null
+    sudo mv jam /usr/local/bin/
+    rm "jam-ubuntu-20.04-x64.tar.gz"
 }
 
 ## Get the destination directory for dev tools.
@@ -37,24 +54,30 @@ if [[ $dest_dir = *[![:ascii:]]* ]]; then
     error "The destination directory contains non-ASCII characters."
 fi
 
-## Install jam.
-echo "[-] Downloading jam..."
-echo "WARNING! At the moment, we may cause issues if you pre-installed jam."
-
-# Check first if we are using Ubuntu/Pop!_OS. Let's support
-# only Ubuntu for now. Contributions welcome.
-. /etc/os-release
-OS=$ID
-OS_LIKE=$ID_LIKE
-
-if [[ $OS != *"ubuntu"* ]] && [[ $OS_LIKE != *"ubuntu"* ]]; then
-    echo "Error: Script does not support non-Ubuntu distributions yet."
-    exit 1
+if [[ $dest_dir != "/"* ]]; then
+    error "The destination directory must be an absolute path."
 fi
 
-wget -q --show-progress "https://github.com/GaijinEntertainment/jam-G8/releases/download/2.5-G8-1.2-2023%2F05%2F04/jam-ubuntu-20.04-x64.tar.gz"
-tar -xvf "jam-ubuntu-20.04-x64.tar.gz"
-sudo mv jam /usr/local/bin/
-rm "jam-ubuntu-20.04-x64.tar.gz"
+## Setup jam.
+echo "[=] Setting up jam..."
+echo -n "- Checking if jam is already installed... "
+if [[ $(command -v jam) > /dev/null ]]; then
+    echo "✅"
+else
+    echo "❌"
+    echo -n "- Downloading jam since it's not installed... "
 
-echo "Finished downloading jam."
+    download_and_install_jam
+
+    echo "✅"
+    echo "- Finished downloading and installing jam."
+
+    echo -n "- Testing jam if it works... "
+    if [[ $(command -v jam) > /dev/null ]]; then
+        echo "✅"
+        echo "- Jam works!"
+    else
+        echo "❌"
+        error "Unable to run jam. Please check your system and/or PATH for conflicts with jam."
+    fi
+fi
