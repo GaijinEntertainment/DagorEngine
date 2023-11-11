5e1be579e081b3c1454fe5e22cc16091443e7239
Sean Francis N. Ballais <sean@seanballais.com>
2023-11-12 02:54:33 +0800

    Added execute permissions to merge_ar on running build_dagor3_cdk_mini.sh.

--- prog/tools/build_dagor3_cdk_mini.sh
+++ prog/tools/build_dagor3_cdk_mini.sh
@@ -7,6 +7,9 @@ if [[ "${TRACE-0}" == "1" ]]; then
     set -o xtrace
 fi
 
+echo "Configure some build-related scripts..."
+chmod +x ../_jBuild/linux64/merge_ar
+
 echo "[NOT PORTED] Build DaEditorX..."
 echo "[NOT PORTED] Build dabuild..."
 echo "[NOT PORTED] Build AssetViewer..."
@@ -16,7 +19,7 @@ echo "[NOT PORTED] Build DDsx plugins..."
 echo "[WIP] Build shader compilers..."
 jam -s Root=../.. -f ../3rdPartyLibs/legacy_parser/dolphin/jamfile
 jam -s Root=../.. -f ../3rdPartyLibs/legacy_parser/whale/jamfile
-#jam -s Root=../.. -f ShaderCompiler2/jamfile-hlsl2spirv
+jam -s Root=../.. -f ShaderCompiler2/jamfile-hlsl2spirv
 
 echo "[NOT PORTED] Build utils..."
 echo "[NOT PORTED] Build GUI tools..."
