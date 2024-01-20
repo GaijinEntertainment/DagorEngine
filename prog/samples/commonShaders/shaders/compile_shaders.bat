3d4c5f58f72a59ca3cb0a3a830144c34c5e089f0
n.savitchev <n.savichev@gaijin.team>
2024-01-20 09:21:43 +0300

    samples/commonShaders: drop broken dynamic_masked_chrome.dshl

--- prog/samples/commonShaders/shaders/compile_shaders.bat
+++
@@ -1,5 +0,0 @@
-@echo on
-call compile_shaders_pc11.bat %1 %2 %3
-call compile_shaders_tools.bat %1 %2 %3
-@echo off
-
--- prog/samples/commonShaders/shaders/compile_shaders_pc11.bat
+++
@@ -1,6 +0,0 @@
-@echo on
-..\..\..\..\tools\dagor3_cdk\util64\dsc2-hlsl11-dev.exe .\shaders_pc11.blk -q -shaderOn -nodisassembly -commentPP -codeDumpErr -bones_start 70  -o ..\..\..\..\_output\sampleShaders\shaders~pc11 %1 %2 %3
-rem copy ..\..\..\..\samples\sampleShaders\game\compiledShaders\game.ps40.shdump.bin ..\..\..\..\samples\planeSample\game\compiledShaders\game.ps40.shdump.bin 
-copy ..\..\..\..\samples\sampleShaders\game\compiledShaders\game.ps40.shdump.bin ..\..\..\..\samples\humanSample\game\compiledShaders\game.ps40.shdump.bin 
-rem copy ..\..\..\..\samples\sampleShaders\game\compiledShaders\game.ps40.shdump.bin ..\..\..\..\samples\shipSample\game\compiledShaders\game.ps40.shdump.bin 
-@echo off
