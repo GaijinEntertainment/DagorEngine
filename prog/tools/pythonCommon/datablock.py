71a26585082f16df80011e06e7a4e95302f5bb7f
n.savitchev <n.savichev@gaijin.team>
2024-06-29 16:12:37 +0300

    updated prog/tools/pythonCommon/datablock.py for dag2blend

--- prog/tools/pythonCommon/datablock.py
+++ prog/tools/pythonCommon/datablock.py
@@ -8,6 +8,22 @@ if sys.version_info[0] >= 3:
 else:
   from pyparsing import *
 
+def getTokensEndLoc():
+    """Method to be called from within a parse action to determine the end
+       location of the parsed tokens."""
+    import inspect
+    fstack = inspect.stack()
+    try:
+        # search up the stack (through intervening argument normalizers) for correct calling routine
+        for f in fstack[2:]:
+            if f[3] == "_parseNoCache":
+                endloc = f[0].f_locals["loc"]
+                return endloc
+        else:
+            raise ParseFatalException("incorrect usage of getTokensEndLoc - may only be called from within a parse action")
+    finally:
+        del fstack
+
 class DataBlock:
     def __init__(self, fname = None, name = None, include_includes = False, preserve_formating = False, root_directory = None):
       self.blocks = []
