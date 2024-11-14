174264803bd299b7cb190a07f53d931f5ff478d5
CodeLikeCXK <44729188+CodeLikeCXK@users.noreply.github.com>
2024-11-14 12:26:38 +0800

    Update build_GITest_WindowsOnArm64.cmd

--- build_GITest_WindowsOnArm64.cmd
+++ build_GITest_WindowsOnArm64.cmd
@@ -1,5 +1,5 @@
 pushd samples\testGI\prog
-jam -sPlatformArch=arm64 -sPlatformSpec=vc17  -D_TARGET_PC_WIN=1
+jam -sPlatformArch=arm64 -sPlatformSpec=vc17  
 cd shaders
 call compile_shaders_dx12_WOA.bat
 call compile_shaders_dx11_WOA.bat
